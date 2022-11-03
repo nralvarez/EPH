3268cadb39c0c8a7448aee4f82c4ba4152bbedfb
Nicolas <74013135+nralvarez@users.noreply.github.com>
2022-11-02 22:48:16 -0300

    Update helpers.R
    Tiene lo del quos, no mucho más

--- tablero_prototipo/helpers.R
+++ tablero_prototipo/helpers.R
@@ -24,11 +24,25 @@ genera_resumen <- function(df){
           (CATEGORIA_OCUPACION == 'CUENTAPROPISTA' & !ES_PROFESIONAL),na.rm=TRUE),
       'TFSR' = sum(
         PONDERA * 
-          (CATEGORIA_OCUPACION == 'TRABAJADORE FLIAR S.R.'),na.rm=TRUE)
+          (CATEGORIA_OCUPACION == 'TRABAJADORE FLIAR S.R.'),na.rm=TRUE),
+      "RESTO_CUENTAPROPISTAS" = CUENTAPROPISTAS - CUENTAPROPISTAS_NO_PROFESIONALES - TFSR,
+      "ASALARIADOS_REGISTRADOS" = sum(
+        PONDERA * (
+          CATEGORIA_OCUPACION == 'ASALARIADE' & REGISTRACION == 1),na.rm=TRUE
+        ),
+      "ASALARIADOS_NOREGISTRADOS" = sum(
+        PONDERA * (
+          CATEGORIA_OCUPACION == 'ASALARIADE' & REGISTRACION == 2),na.rm=TRUE
+      ),
+      "PATRONES" = sum(
+        PONDERA * (
+          CATEGORIA_OCUPACION == 'PATRON'), na.rm=TRUE
+        ),
+      "EP" = CUENTAPROPISTAS_NO_PROFESIONALES + TFSR
       ) %>% 
     return()
 }
-
+view(individual_03.hoy)
 ############################
 ## FUNCIONES PARA EL TABLERO
 ############################
@@ -76,26 +90,99 @@ genera_aes_cantTrabEP_plot <- function(input){
 
 genera_grouping_vars_cantTrabEP_plot <- function(input){
   #'@description Esta función genera el agrupamiento necesario para el plot cantTrabEP_plot
-  if(input$separar_sexos){ # Si separamos por sexo
-    if(input$separar_zonas){
-    
-      grouping_vars <- quos('SEXO','YEAR','TRIMESTER', input$variable_zona)
-      
-    }else{
-      
-      grouping_vars <- quos('SEXO','YEAR','TRIMESTER')
-      
-    }
-  }else {
-    if(input$separar_zonas){
-      
-      grouping_vars <- quos('YEAR','TRIMESTER', input$variable_zona)
-      
-    }else{
-      
-      grouping_vars <- quos('YEAR','TRIMESTER')
-      
-    }
+  # if(input$separar_sexos){ # Si separamos por sexo
+  # 
+  #   if(input$separar_zonas){ # Si separamos por zona
+  # 
+  #     grouping_vars <- quos('SEXO','YEAR','TRIMESTER', input$variable_zona)
+  # 
+  #     if(input$agregar_ocupados){ #Si separamos por ocupación
+  # 
+  #       grouping_vars <- quos('SEXO','YEAR','TRIMESTER', input$variable_zona, "EP", "RESTO_CUENTAPROPISTAS", "ASALARIADOS_REGISTRADOS", "ASALARIADOS_NOREGISTRADOS", "PATRONES")
+  # 
+  #     }
+  # 
+  #   }else{
+  # 
+  #     grouping_vars <- quos('SEXO','YEAR','TRIMESTER')
+  # 
+  #     if(input$agregar_ocupados){ #Si separamos por ocupación
+  # 
+  #       grouping_vars <- quos('SEXO','YEAR','TRIMESTER', "EP", "RESTO_CUENTAPROPISTAS", "ASALARIADOS_REGISTRADOS", "ASALARIADOS_NOREGISTRADOS", "PATRONES")
+  # 
+  #     }
+  # 
+  #   }
+  # }else {
+  #   if(input$separar_zonas){ # Si separamos por zona
+  # 
+  #     grouping_vars <- quos('YEAR','TRIMESTER', input$variable_zona)
+  # 
+  #     if(input$agregar_ocupados){ #Si separamos por ocupación
+  # 
+  #       grouping_vars <- quos('YEAR','TRIMESTER', input$variable_zona, "EP", "RESTO_CUENTAPROPISTAS", "ASALARIADOS_REGISTRADOS", "ASALARIADOS_NOREGISTRADOS", "PATRONES")
+  # 
+  #     }
+  # 
+  #   }else{
+  # 
+  #     if(input$agregar_ocupados){ #Si separamos por ocupación
+  # 
+  #       grouping_vars <- quos('YEAR','TRIMESTER', "EP", "RESTO_CUENTAPROPISTAS", "ASALARIADOS_REGISTRADOS", "ASALARIADOS_NOREGISTRADOS", "PATRONES")
+  # 
+  #     }else{ # Si no separamos
+  # 
+  #       grouping_vars <- quos('YEAR','TRIMESTER')
+  # 
+  #     }
+  # 
+  #   }
+  # }
+ 
+  
+  #########################################################
+  
+  ###POR LAS DUDAS DEJO ACÁ LO QUE ESTABA ESCRITO ANTES####
+  # if(input$separar_sexos){ # Si separamos por sexo
+  # 
+  #   if(input$separar_zonas){
+  # 
+  #     grouping_vars <- quos('SEXO','YEAR','TRIMESTER', input$variable_zona)
+  # 
+  #   }else{
+  # 
+  #     grouping_vars <- quos('SEXO','YEAR','TRIMESTER')
+  # 
+  #   }
+  # }else {
+  #   if(input$separar_zonas){
+  # 
+  #     grouping_vars <- quos('YEAR','TRIMESTER', input$variable_zona)
+  # 
+  #   }else{
+  # 
+  #     grouping_vars <- quos('YEAR','TRIMESTER')
+  # 
+  #   }
+  # }
+
+  
+  ########################################################
+  
+  ### INTENTO DE NICO POR HACER MENOS LÍNEAS. Cuando intentaba correr la aplicación me decía que había un vector vacío, al cambiar el codigo anterior por el que escribí arriba, dicho problema desapareció.
+
+  ### Ya funciona bien las primeras dos lineas, lo de ocupados todavia no porque no esta implementado el grafico
+
+  agrupar <- c("YEAR", "TRIMESTER")
+  if(input$separar_sexos){
+    agrupar <- c(agrupar, "SEXO")
   }
-  return(grouping_vars)
+  if(input$separar_zonas){
+    agrupar<- c(agrupar, input$variable_zona)
+  }
+  if(input$agregar_ocupados){
+    agrupar <- c(agrupar, "EP", "RESTO_CUENTAPROPISTAS", "ASALARIADOS_REGISTRADOS", "ASALARIADOS_NOREGISTRADOS", "PATRONES")
+  }
+  grouping_vars <- quos(agrupar) 
+  return(grouping_vars) 
 }
